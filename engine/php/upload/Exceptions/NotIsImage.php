dd365dcf8f6518a3a63f7cfb1ff8868ad5bf8456
maximiliano garcia <maximiliano_garcia93@hotmail.com>
2021-12-07 15:24:25 -0300

    fixe bug in exceptions

--- engine/php/upload/Exceptions/NotIsImage.php
+++ engine/php/upload/Exceptions/NotIsImage.php
@@ -2,12 +2,12 @@
 
 namespace upload\Exceptions;
 
-use ExceptionUpload;
+
 
 class NotIsImage extends ExceptionUpload
 {
     function __construct($type = "", $code = 400)
     {
-        parent::__construct("Selecciono el typo imagen pero el archivo leido no es una imagen($type)", $code);
+        parent::__construct("Selecciono el typo imagen pero el archivo leido no es una imagen, formato recibido: $type", $code);
     }
 }
